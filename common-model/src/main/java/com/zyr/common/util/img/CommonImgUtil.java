1eb3a8ebf4abe6908ca97502ad80f2c7e16bf026
zhengyongrui <zyr@outlook.com>
2020-02-29 17:11:19 +0800

    修复gif图片等bug

--- common-model/src/main/java/com/zyr/common/util/img/CommonImgUtil.java
+++ common-model/src/main/java/com/zyr/common/util/img/CommonImgUtil.java
@@ -2,10 +2,15 @@ package com.zyr.common.util.img;
 
 import cn.hutool.core.img.Img;
 import cn.hutool.core.img.ImgUtil;
+import cn.hutool.core.io.IORuntimeException;
 import cn.hutool.core.util.NumberUtil;
+import org.springframework.http.MediaType;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.io.InputStream;
 
 /**
@@ -26,7 +31,7 @@ public class CommonImgUtil extends ImgUtil {
     public static Image compress(InputStream srcStream, int width, Color fixedColor) {
         BufferedImage bufferedImage = read(srcStream);
         int srcWidth = bufferedImage.getWidth();
-        int srcHeight = bufferedImage.getWidth();
+        int srcHeight = bufferedImage.getHeight();
         double widthRatio = NumberUtil.div(width, srcWidth);
         Img srcImg = Img.from(bufferedImage);
         Img resultImg;
@@ -37,6 +42,30 @@ public class CommonImgUtil extends ImgUtil {
             return bufferedImage;
         }
         return resultImg.getImg();
+    }
+
+    /**
+     * 从流中读取图片(兼容gif)
+     *
+     * @param multipartFile 图片文件
+     * @return 图片
+     * @since 3.2.2
+     */
+    public static BufferedImage read(MultipartFile multipartFile) {
+        if (!MediaType.IMAGE_GIF_VALUE.equals(multipartFile.getContentType())) {
+            try {
+                return ImageIO.read(multipartFile.getInputStream());
+            } catch (IOException e) {
+                throw new IORuntimeException(e);
+            }
+        } else {
+            try {
+                final GifDecoder.GifImage gifImage = GifDecoder.read(multipartFile.getInputStream());
+                return new BufferedImage(gifImage.getWidth(), gifImage.getHeight(), BufferedImage.TYPE_BYTE_INDEXED);
+            } catch (IOException e) {
+                throw new IORuntimeException(e);
+            }
+        }
     }
 
 }
